e814785a4e202601ec411e0fae276238a9877aef
Adam Wadsworth <adam_y2005@hotmail.com>
2023-06-14 00:34:43 +0100

    cards procedualy generated

--- client/src/Componants/Mob/ForcastCard.js
+++ client/src/Componants/Mob/ForcastCard.js
@@ -2,16 +2,18 @@ import React from 'react';
 import "../CSS/Mob/ForcastCardMob.css"
 
 
-const ForcastCard = ({ data }) => {
+const ForcastCard = ({ data, className }) => {
+
+    console.log(data)
     return (
-        <div className="card col">
-            <img src={data.current.condition.icon} style={{ width: "64px", height: "64px", borderRadius:"0px"}} className="card-img-top" alt="Weather icon"/>
+        <div className={className + " card col"}>
+            <img src={data.condition.icon} style={{ width: "64px", height: "64px", borderRadius:"0px"}} className="card-img-top" alt="Weather icon"/>
             <div className="card-body">
-                <h5 className="card-title">16:00</h5>
-                <p style={{fontSize:"20px"}} className="card-text"><strong>{data.forecast.forecastday[0].hour[16].temp_c}°C</strong></p>
-                <p className="card-text">Feels:  {data.current.feelslike_c}°C</p>
-                <p className="card-text">Hum: {data.current.humidity}%</p>
-                <p className="card-text">Precip: {data.forecast.forecastday[0].day.totalprecip_mm}mm</p>
+                <h5 className="card-title">{data.time.split(" ")[1]}</h5>
+                <p style={{fontSize:"20px"}} className="card-text"><strong>{data.temp_c}°C</strong></p>
+                <p className="card-text">Feels:  {data.feelslike_c}°C</p>
+                <p className="card-text">Hum: {data.humidity}%</p>
+                <p className="card-text">Precip: {data.precip_mm}mm</p>
             </div>
         </div>
     );
